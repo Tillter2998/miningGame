baf93e041ddd27aa456e553222942b7620733f81
Tillter2998 <elrich.dev2998@gmail.com>
2024-06-28 17:45:20 -0300

    Add game engine

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
+)
+
+type Game struct{}
+
+func (g *Game) Update(screen *ebiten.Image) error {
+	if ebiten.IsDrawingSkipped() {
+		return nil
+	}
+
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hello, World!")
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 320, 240
+}
+
+func main() {
+	game := &Game{}
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Mining Game")
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
+}
