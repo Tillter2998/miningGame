a181cd85694af5513c0c27879facea7b38bac978
Tillter2998 <elrich.dev2998@gmail.com>
2024-06-30 15:53:52 -0300

    Add player image and render it

--- main.go
+++ main.go
@@ -10,9 +10,18 @@ import (
 var (
 	err        error
 	background *ebiten.Image
+	miner      *ebiten.Image
+	playerOne  player
 )
 
 type Game struct{}
+
+type player struct {
+	image *ebiten.Image
+	xPos  float64
+	yPos  float64
+	speed float64
+}
 
 func (g *Game) Update(screen *ebiten.Image) error {
 	if ebiten.IsDrawingSkipped() {
@@ -26,6 +35,10 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Translate(0, 0)
 	screen.DrawImage(background, op)
+
+	playerOp := &ebiten.DrawImageOptions{}
+	playerOp.GeoM.Translate(playerOne.xPos, playerOne.yPos)
+	screen.DrawImage(playerOne.image, playerOp)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -35,8 +48,14 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 func init() {
 	background, _, err = ebitenutil.NewImageFromFile("assets/background.png", ebiten.FilterDefault)
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
+	miner, _, err = ebitenutil.NewImageFromFile("assets/Miner.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	playerOne = player{miner, 640 / 2, 480 / 2, 4}
 }
 
 func main() {
